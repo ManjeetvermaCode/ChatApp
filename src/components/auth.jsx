26ee0d5e56ef9f5e6738bbd14a498c84c90c4087
manjeetverma <manjeetverma@gmail.com>
2023-07-16 21:58:13 +0530

    added react-cookie to the app

--- src/components/auth.jsx
+++ src/components/auth.jsx
@@ -1,9 +1,19 @@
+import {Cookies} from 'react-cookie'
+const cookies=new Cookies()
+
 import {signInWithPopup, } from 'firebase/auth'
 import {provider,auth} from '../firebase-config'
 export default function Auth(){
     
     const SignInWGoogle=async()=>{
-        const result=await signInWithPopup(auth,provider)
+        try {
+            const result=await signInWithPopup(auth,provider)
+            console.log(result)
+            cookies.set('user-token',result.user.refreshToken)//The refreshToken is used to obtain a new access token when the current one expires, without requiring the user to reauthenticate.
+            
+        } catch (error) {
+            console.log(error)
+        }
     }
     
     return (
