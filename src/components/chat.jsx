5e7ff7474138c151ba836fa684894efdc3c010a4
manjeetverma <manjeetverma@gmail.com>
2023-07-23 18:12:54 +0530

    added a useeffect hook that will retrieves the data from the database, and assign it to a hook for displahing data

--- src/components/chat.jsx
+++ src/components/chat.jsx
@@ -1,10 +1,23 @@
-import { serverTimestamp, addDoc, collection } from "firebase/firestore"
-import { useState } from "react"
+import { serverTimestamp, addDoc, collection, query, where, onSnapshot } from "firebase/firestore"
+import { useEffect, useState } from "react"
 import {auth,db} from '../firebase-config'
 
 export default function Chat({room}){
 const messageRef=collection(db,'messages')//used for retrieving collection
 const [msg,setmsg]=useState("")
+const [allmsgs,setallmsgs]=useState([])
+
+useEffect(()=>{
+    const queryMessages=query(messageRef,where("room", "==", room));
+    const unsuscribe=onSnapshot(queryMessages,(snapshot)=>{
+        let messages=[];
+        snapshot.forEach((doc)=>{
+            messages.push({...doc.data(),id:doc.id})
+        })
+        setallmsgs(messages)
+    })
+    return ()=>unsuscribe()
+},[])
 
 const handlesubmit=async(e)=>{
     e.preventDefault()
@@ -22,6 +35,7 @@ const handlesubmit=async(e)=>{
 
     return (
         <>
+        <div> {allmsgs.map((msg)=><h1 key={crypto.randomUUID()}>{msg.text}</h1>)}</div>
             <form onSubmit={handlesubmit}>
                 <input type="text" placeholder="enter message" onChange={(e)=>{setmsg(e.target.value)}} value={msg} />
                 <button type="submit">Submit</button>
